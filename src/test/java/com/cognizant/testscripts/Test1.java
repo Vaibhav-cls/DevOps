a446806282c78f86ef60da58967467ba8e253934
Vaibhav-cls <vaibhavghanekar19@gmail.com>
2025-06-30 07:54:44 +0530

    first commit

---
+++ src/test/java/com/cognizant/testscripts/Test1.java
@@ -0,0 +1,11 @@
+package com.cognizant.testscripts;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("This is test 1");
+	}
+}
---
+++ src/test/java/com/cognizant/testscripts/Test2.java
@@ -0,0 +1,11 @@
+package com.cognizant.testscripts;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void test2() {
+		System.out.println("This is test 2");
+	}
+}
